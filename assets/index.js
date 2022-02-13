791fd6496b583f813bc9ee5d50df90d8371b1556
Sooova <83949034+Sooova@users.noreply.github.com>
2022-02-13 18:42:22 +1100

    Initial Commit
    Baseline framework there. Mobile functionality is not fixed, mobile is broken.

---
+++ assets/index.js
@@ -0,0 +1,48 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".imgGalery");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".horizontalScrollDiv",
+    // start: 'top top+=100',
+    pin: false,
+    scrub: 1,
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=1750",
+    markers: true,
+  }
+});
+
+// let sections2 = gsap.utils.toArray(".textGallery");
+// gsap.to(sections2, {
+//     xPercent: 80 * (sections.length - 1),
+//     ease: "none",
+//     scrollTrigger: {
+//       trigger: ".dare",
+//       start: 'top top+=100',
+//       pin: false,
+//       scrub: 1,
+//       // base vertical scrolling on how wide the container is so it feels more natural.
+//       end: "+=1500",
+//       markers: true,
+//     }
+//   });
+
+  
+let sections3 = gsap.utils.toArray(".imgGalery2");
+gsap.to(sections3, {
+    xPercent: 100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".horizontalScrollDiv",
+    //   start: 'top top+=250',
+      pin: false,
+      scrub: 1,
+      // base vertical scrolling on how wide the container is so it feels more natural.
+      end: "+=3050",
+      markers: true,
+    }
+  });
